8f76304fe7e9ab21628c8465f4959dd105af6fed
Maxime EY <maxime.ey@viacesi.fr>
2022-03-18 17:00:50 +0100

    commit

--- index.php
+++ index.php
@@ -12,37 +12,47 @@ session_start();
     <script type="text/javascript" src="effets.js"></script>
 </head>
 <body>
+<?php 
+if (isset($_SESSION['login']))
+{
+    echo "You are connected as: " . $_SESSION['login'];
+}
+else
+{
+    ?>
+    <div class="limiter">
 
-<div class="limiter">
+<div class="container-login100">
+    <div class="wrap-login100">
+        <div class="login100-form-title">
+            <span class = "login100-form-title-1">
+                Sign in
+            </span>
+        </div>
 
-    <div class="container-login100">
-        <div class="wrap-login100">
-            <div class="login100-form-title">
-                <span class = "login100-form-title-1">
-                    Sign in
-                </span>
+
+        <form class="login100-form validate-form" method="POST" action = "login.php">
+             <div class="wrap-input100 validate-input m-b-26" data-validate="E-mail is required">
+                   <span class = "label-input100">E-mail</span>
+                   <input class = "input100" type="text" name="login" placeholder="Enter e-mail">
+                  <span class = "focus-input100"></span>
+             </div>
+
+              <div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
+                    <span class = "label-input100">Password</span>
+                       <input class = "input100" type="password" name="password" placeholder="Enter password">
+                      <span class = "focus-input100"></span>
             </div>
 
-
-            <form class="login100-form validate-form" method="POST" action = "login.php">
-                 <div class="wrap-input100 validate-input m-b-26" data-validate="E-mail is required">
-                       <span class = "label-input100">E-mail</span>
-                       <input class = "input100" type="text" name="login" placeholder="Enter e-mail">
-                      <span class = "focus-input100"></span>
-                 </div>
-
-                  <div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
-                        <span class = "label-input100">Password</span>
-                           <input class = "input100" type="password" name="password" placeholder="Enter password">
-                          <span class = "focus-input100"></span>
-                </div>
-
-                 <div class = "container-login100-form-btn">
-                        <button class = "login100-form-btn" name="submit">
-                             Login
-                        </button>
-                </div>
-            </form>
-        </div>
+             <div class = "container-login100-form-btn">
+                    <button class = "login100-form-btn" name="submit">
+                         Login
+                    </button>
+            </div>
+        </form>
     </div>
 </div>
+</div>
+    <?
+};
+?>
