8a95288d6499ac748b48fe26014fb58913d33470
Maxime EY <maxime.ey@viacesi.fr>
2022-03-19 21:01:18 +0100

    commit

---
+++ interact_with_an_account.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Interact with an account</h1>
+
+
+    <h3>Interact with an account of:</h3>
+
+    <a href="interact_company.php">a company</a>
+<br>
+    <a href="interact_student.php">a student</a>
+<br>
+    <a href="interact_representative.php">a representative</a>
+<br>
+    <a href="interact_pilot.php">a pilot</a>
+    
+</body>
+</html>
