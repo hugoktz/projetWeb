6b6c13c0e7a53e43899fb9e4557d25d180a6065e
Maxime EY <maxime.ey@viacesi.fr>
2022-03-29 10:39:43 +0200

    commit

--- login_representative.php
+++ login_representative.php
@@ -25,7 +25,7 @@
 
 <?php
     include 'generate_cookie.php';
-    $connect = mysqli_connect("127.0.0.1","root","","web_project") or die("Connection failed");
+    $connect = mysqli_connect("127.0.0.1","root","","web_project_try_5") or die("Connection failed");
     if(!empty($_POST['save']))
     {
         $username = $_POST['login'];
